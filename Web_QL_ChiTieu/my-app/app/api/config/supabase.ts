02a597ebe180077cb58fec4adfb0db67925d4de0
TranDinhTu-22CT1 <tu_2251220199@dau.edu.vn>
2026-04-24 09:37:13 +0700

    Update project backend (Backend đã xong, đã nối frontend với backend)

---
+++ Web_QL_ChiTieu/my-app/app/api/config/supabase.ts
@@ -0,0 +1,12 @@
+// app/api/config/supabase.ts
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+import path from 'path';
+
+// Ép Next.js lùi ra ngoài 1 thư mục để đọc chung file .env với Frontend
+dotenv.config({ path: path.resolve(process.cwd(), '../.env') });
+
+const supabaseUrl = process.env.SUPABASE_URL || '';
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY || '';
+
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
