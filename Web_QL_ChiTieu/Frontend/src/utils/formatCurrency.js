071fcb9168a268e2a5e7dee8b336950670a7e320
TranDinhTu-22CT1 <tu_2251220199@dau.edu.vn>
2026-04-24 09:42:51 +0700

    Revert "Update project backend (Backend đã xong, đã nối frontend với backend)"
    This reverts commit 02a597ebe180077cb58fec4adfb0db67925d4de0.

--- Web_QL_ChiTieu/Frontend/src/utils/formatCurrency.js
+++
@@ -1,7 +0,0 @@
-export const formatCurrency = (value) => {
-  return new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-    maximumFractionDigits: 0,
-  }).format(value)
-}
